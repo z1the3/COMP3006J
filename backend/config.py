e747c9a0fb6d8f0d29b8707edb5efbe12da1df08
pizzas17 <sun13718780917@gmail.com>
2025-05-04 21:57:44 +0800

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,8 @@ LOCAL_IMAGE_DIR = os.path.join(os.getcwd(), "images")
 os.makedirs(LOCAL_IMAGE_DIR, exist_ok=True)
 
 # OSS 配置（需完善，替换成咱们的oss信息）
-OSS_ENDPOINT = os.getenv("OSS_ENDPOINT", "oss-cn-hangzhou.aliyuncs.com")
-OSS_BUCKET_NAME = os.getenv("OSS_BUCKET_NAME", "your-bucket-name")
-OSS_ACCESS_KEY_ID = os.getenv("OSS_ACCESS_KEY_ID", "your-access-key-id")
-OSS_ACCESS_KEY_SECRET = os.getenv("OSS_ACCESS_KEY_SECRET", "your-access-key-secret")
+OSS_ENDPOINT = "oss-cn-beijing.aliyuncs.com"
+OSS_BUCKET_NAME = "comp3006j-bucket"
+OSS_ACCESS_KEY_ID = "LTAI5tGny7d2pZYMoJZyVPoj"
+OSS_ACCESS_KEY_SECRET = "ohWi6t4QsB56WEsUqCiAhZO6faW0dn"
+
